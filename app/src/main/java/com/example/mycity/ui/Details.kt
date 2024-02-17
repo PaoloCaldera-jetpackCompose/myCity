cf8fb3830ce351b0c5c4bfff69e8f771b402ea91
PaoloCaldera <paolo.caldera@outlook.com>
2024-02-17 17:24:12 +0100

    feature: improve the DetailsScreen layout
    Add cards for improving the layout, around the image, around the text description, and around the address, which is now at the bottom of the page.
    
    Integrates #3

--- app/src/main/java/com/example/mycity/ui/Details.kt
+++ app/src/main/java/com/example/mycity/ui/Details.kt
@@ -1,11 +1,17 @@
 package com.example.mycity.ui
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,30 +29,47 @@ import com.example.mycity.ui.theme.MyCityTheme
 @Composable
 fun DetailsScreen(modifier: Modifier = Modifier) {
     Column(
+        verticalArrangement = Arrangement.SpaceBetween,
         modifier = Modifier
-            .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
+            .padding(8.dp)
+            .fillMaxSize()
     ) {
-        Image(
-            painter = painterResource(R.drawable.breakfast_1_image),
-            contentDescription = null,
-            contentScale = ContentScale.FillWidth,
-            modifier = Modifier.fillMaxWidth()
-        )
-        Text(
-            text = stringResource(R.string.breakfast_1_description),
-            textAlign = TextAlign.Justify,
-            style = MaterialTheme.typography.bodyMedium,
-            modifier = Modifier.padding(16.dp)
-        )
-        Text(
-            text = stringResource(R.string.general_address, stringResource(R.string.breakfast_1_address)),
-            textAlign = TextAlign.End,
-            style = MaterialTheme.typography.labelSmall,
+        Card {
+            Image(
+                painter = painterResource(R.drawable.breakfast_1_image),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Card(
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
             modifier = Modifier
-                .padding(end = 16.dp, bottom = 16.dp)
-                .align(Alignment.End)
-        )
+                .weight(1f)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Text(
+                text = stringResource(R.string.breakfast_1_description),
+                textAlign = TextAlign.Justify,
+                style = MaterialTheme.typography.bodyMedium,
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(8.dp))
+        Card(
+            colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.tertiaryContainer),
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(
+                text = stringResource(R.string.general_address, stringResource(R.string.breakfast_1_address)),
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.labelSmall,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
     }
 }
 
