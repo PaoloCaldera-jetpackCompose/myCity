d8656b8dc7d771f5f6425108f2b4618a8a7f10d8
PaoloCaldera <paolo.caldera@outlook.com>
2024-02-17 21:15:51 +0100

    feature: build the layout of the category screen
    Use the CategoryEntriesScreen composable to build the list and the CategoryEntryItem to build the item.
    Add the star icon to indicate the rate inside the item card.
    
    Integrates #5

--- app/src/main/java/com/example/mycity/ui/CategoryEntries.kt
+++ app/src/main/java/com/example/mycity/ui/CategoryEntries.kt
@@ -1,9 +1,119 @@
 package com.example.mycity.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mycity.Datasource
+import com.example.mycity.R
+import com.example.mycity.model.Entry
+import com.example.mycity.ui.theme.MyCityTheme
 
 @Composable
-fun CategoryEntriesScreen(modifier: Modifier = Modifier) {
+fun CategoryEntriesScreen(entries: List<Entry>, modifier: Modifier = Modifier) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        items(entries) {
+            CategoryEntryItem(entry = it, modifier = modifier)
+        }
+    }
+}
 
+@Composable
+fun CategoryEntryItem(entry: Entry, modifier: Modifier = Modifier) {
+    Card(modifier = Modifier
+        .height(128.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            Column {
+                Text(
+                    text = stringResource(entry.name),
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.titleLarge,
+                )
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Text(
+                        text = stringResource(entry.rate),
+                        textAlign = TextAlign.Start,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Icon(
+                        painter = painterResource(R.drawable.ic_star_rate),
+                        contentDescription = null
+                    )
+                }
+                Text(
+                    text = stringResource(entry.address).substringBefore(","),
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.labelSmall
+                )
+            }
+            Image(
+                painter = painterResource(entry.image),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(8.dp))
+                    .width(128.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CategoryEntriesScreenPreview() {
+    MyCityTheme {
+        CategoryEntriesScreen(
+            entries = Datasource.getEntries(1)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryEntryItemPreview() {
+    MyCityTheme {
+        CategoryEntryItem(
+            entry = Entry(
+                id = 31,
+                name = R.string.beach_3_name,
+                rate = R.string.beach_3_rate,
+                address = R.string.beach_3_address,
+                description = R.string.beach_3_description,
+                image = R.drawable.beach_3_image,
+            ),
+        )
+    }
 }
